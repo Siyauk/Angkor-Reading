fd60d5d392078c8bc55a04c6ccd46da4075851a2
Angkor-Reading <110264568+Siyauk@users.noreply.github.com>
2022-08-01 18:44:40 -0500

    3

--- script.js
+++ script.js
@@ -109,15 +109,15 @@ var swiper = new Swiper(".books-slider", {
     
             428: {
                 slidesPerView: 1,
-               
+                slidesPerView: 2,
                 
             },
             926: {
-                slidesPerView: 2,
                 slidesPerView: 3,
                 slidesPerView: 4,
                 slidesPerView: 5,
                 slidesPerView: 6,
+                slidesPerView: 7,
                 
             },
     },
