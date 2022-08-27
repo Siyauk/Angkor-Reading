73ba4398a0af944481bfbef13613ddbcd2adae62
Angkor-Reading <110264568+Siyauk@users.noreply.github.com>
2022-08-26 19:00:04 -0500

    hj;kl

--- script.js
+++ script.js
@@ -13,7 +13,37 @@ document.querySelector('#login-btn').onclick = () => {
 document.querySelector('#close-login-btn').onclick = () => {
     loginForm.classList.remove('active');
 }
+window.onscroll = () =>{
 
+    searchForm.classList.remove('active');
+  
+    if(window.scrollY > 80){
+      document.querySelector('.header .header-2').classList.add('active');
+    }else{
+      document.querySelector('.header .header-2').classList.remove('active');
+    }
+  
+  }
+  
+  window.onload = () =>{
+  
+    if(window.scrollY > 80){
+      document.querySelector('.header .header-2').classList.add('active');
+    }else{
+      document.querySelector('.header .header-2').classList.remove('active');
+    }
+  
+    fadeOut();
+  
+  }
+  
+  function loader(){
+    document.querySelector('.loader-container').classList.add('active');
+  }
+  
+  function fadeOut(){
+    setTimeout(loader, 4000);
+  }
 
 function myFunction1() {
     var dots = document.getElementById("dots1");
