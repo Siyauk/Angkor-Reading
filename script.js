cb6f97d238e63dd1ef7211006eb677be040f7b50
Angkor-Reading <110264568+Siyauk@users.noreply.github.com>
2022-10-16 16:52:24 -0500

    sefew

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
 searchForm = document.querySelector('.search-form');
 
-document.querySelector('#search-btn').onclick = () => {
-    searchForm.classList.toggle('active');
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
 }
 
 let loginForm = document.querySelector('.login-form-container');
 
-document.querySelector('#login-btn').onclick = () => {
-    loginForm.classList.toggle('active');
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
 }
 
-document.querySelector('#close-login-btn').onclick = () => {
-    loginForm.classList.remove('active');
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+
+  searchForm.classList.remove('active');
+
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
+  }
+
+}
+
+window.onload = () =>{
+
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
+  }
+
+  fadeOut();
+
+}
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeOut(){
+  setTimeout(loader, 4000);
 }
 function myFunction1() {
     var dots = document.getElementById("dots1");
